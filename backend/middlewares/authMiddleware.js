080d6d3f6438ce48491b0bd801cb11b1f09e3d66
Juan Camilo Garcia <juankg19@gmail.com>
2025-10-08 20:59:07 -0500

    feat:Middleware para proteger rutas y Rutas

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET ;
+
+export const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "No hay token, autorización denegada" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ MessagePort: "Token no válido" });
+  }
+};
+
+// Solo vendedores
+export const sellerOnly = (req, res, next) => {
+  if (req.user.role !== "vendedor") {
+    return res.status(403).json({ msg: "Acceso denegado: solo vendedores" });
+  }
+  next();
+};
