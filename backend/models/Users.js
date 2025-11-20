9adaf02fa74768b8d0835883e92a3c8a9fc16650
Juan Camilo Garcia <juankg19@gmail.com>
2025-11-19 21:37:44 -0500

    feat:logica y componente producto por id

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -18,6 +18,12 @@ const userSchema = new mongoose.Schema({
         enum:["comprador","vendedor"],
         default:"comprador"
     },
+    cart:[
+        {
+            productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product"},
+            quantity:{type:Number,default:1}
+        }
+    ]
 
 },{timestamps:true})
 
