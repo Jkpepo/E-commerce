48a9d25221597aa20ddd8f6df65fd573aab4bb22
Juan Camilo Garcia <juankg19@gmail.com>
2025-12-02 20:17:42 -0500

    feat: incrementar, disminuir y eliminar productos del carrito desde el backend

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -1,49 +1,42 @@
 import Users from "../models/Users.js";
 
-export const addToCart = async (req,res)=>{
-    
-        console.log(req.body)
-    try{
-        const userId = req.user.id
-        const {productId} = req.body;
-        const user = await Users.findById(userId);
-        if(!user){
-            return res.status(404).json({
-                message:"Usuario no encontrado"
-            });
-        }
-        if(!user.cart){
-            user.cart =[];
-        }
-        const item = user.cart.find(
-            (p)=> p.productId.toString() === productId
-        );
-
-        if(item){
-            item.quantity +=1;
-        }else{
-            user.cart.push({productId,quantity:1});
-        }
-        await user.save();
-
-         const updatedUser = await Users.findById(userId)
-      .populate("cart.productId");
-
-
-
-        res.status(200).json({
-            message: "Producto agregado al carrito",
-            cart:updatedUser.cart,
-        })
-    }catch(error){
-        console.log(error)
-        res.status(500).json({
-            message:"Error al agregar el producto al carrito",
-            error:error.message,
-        });
+export const addToCart = async (req, res) => {
+  console.log(req.body);
+  try {
+    const userId = req.user.id;
+    const { productId } = req.body;
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
     }
+    if (!user.cart) {
+      user.cart = [];
+    }
+    const item = user.cart.find((p) => p.productId.toString() === productId);
 
-}
+    if (item) {
+      item.quantity += 1;
+    } else {
+      user.cart.push({ productId, quantity: 1 });
+    }
+    await user.save();
+
+    const updatedUser = await Users.findById(userId).populate("cart.productId");
+
+    res.status(200).json({
+      message: "Producto agregado al carrito",
+      cart: updatedUser.cart,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error al agregar el producto al carrito",
+      error: error.message,
+    });
+  }
+};
 
 export const getCart = async (req, res) => {
   try {
@@ -54,5 +47,84 @@ export const getCart = async (req, res) => {
     res.status(200).json({ cart: user.cart });
   } catch (error) {
     res.status(500).json({ message: "Error al obtener carrito" });
+  }
+};
+
+export const decrementProduct = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { productId } = req.body;
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
+    }
+    const item = user.cart.find((p) => p.productId.toString() === productId);
+
+    if (!item) {
+      return res.status(404).json({
+        message: "Prodcuto no encontrado en el carrito",
+      });
+    }
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+    }
+    await user.save();
+    await user.populate("cart.productId");
+    res.json({ cart: user.cart });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error decrementando producto" });
+  }
+};
+
+export const incrementProduct = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { productId } = req.body;
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
+    }
+    const item = user.cart.find((p) => p.productId.toString() === productId);
+
+    if (!item) {
+      return res.status(404).json({
+        message: "Prodcuto no encontrado en el carrito",
+      });
+    }
+    if (item.quantity >= 1) {
+      item.quantity += 1;
+    }
+    await user.save();
+    await user.populate("cart.productId");
+    res.json({ cart: user.cart });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error incrementando  producto" });
+  }
+};
+
+export const cleanProduct = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { productId } = req.body;
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
+    }
+    user.cart = user.cart.filter((p) => p.productId.toString() !== productId);
+
+    await user.save();
+    await user.populate("cart.productId");
+    res.json({ cart: user.cart });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error incrementando  producto" });
   }
 };
--- backend/routes/CartRoutes.js
+++ backend/routes/CartRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { addToCart,getCart } from "../controllers/CartController.js";
+import { addToCart,getCart,decrementProduct,incrementProduct,cleanProduct } from "../controllers/CartController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/addcart", authMiddleware , addToCart);
 router.get("/", authMiddleware, getCart);
+router.post("/decrementproduct", authMiddleware, decrementProduct);
+router.post("/incrementproduct", authMiddleware, incrementProduct);
+router.delete("/cleanproduct", authMiddleware, cleanProduct);
 
 export default router;
